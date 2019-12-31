6f91339321b1c2c8c218d9abdab62a60ea9716ce
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2019-12-31 15:10:13 +0530

    add object creation and method calling

--- src/methodOverloadingDemo/Main.java
+++ src/methodOverloadingDemo/Main.java
@@ -3,6 +3,10 @@ package methodOverloadingDemo;
 public class Main {
     public static void main(String[] args) {
 
+        // creating object of class and using its constructor
+        ConversionToCentimeters obj = new ConversionToCentimeters(1, 1);
 
+        // printing output result by calling method
+        System.out.println(obj.calcFeetAndInchesToCentimeters());
     }
 }
