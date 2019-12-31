75bdc0ae6f69faf16b35afc81cf119c42aa40c39
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2019-12-31 14:43:35 +0530

    add constructor

--- src/methodOverloadingDemo/ConversionToCentimeters.java
+++ src/methodOverloadingDemo/ConversionToCentimeters.java
@@ -1,4 +1,13 @@
 package methodOverloadingDemo;
 
 public class ConversionToCentimeters {
+
+    private double feet;
+    private double inches;
+
+    // constructor
+    public ConversionToCentimeters(double feet, double inches) {
+        this.feet = feet;
+        this.inches = inches;
+    }
 }
