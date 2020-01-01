4f09be7787a2da81d415b8fc5d1b39718faf8205
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-01 15:59:13 +0530

    improved code readability

--- src/methodOverloadingDemo/ConversionToCentimeters.java
+++ src/methodOverloadingDemo/ConversionToCentimeters.java
@@ -11,30 +11,32 @@ public class ConversionToCentimeters {
         this.inches = inches;
     }
 
-    // method to convert feet and inches to centimeters
-    public double calcFeetAndInchesToCentimeters(double feet, double inches) {
-        if (feet >= 0 && inches >= 0 && inches < 12) {
+    // overloaded method using values given through constructor
+    public double calcFeetAndInchesToCentimeters() {
+        if (feet >= 0 && inches >= 0 && inches <= 12) {
             return ((feet * 12) * 2.54) + (inches * 2.54);
         } else {
             return -1;
         }
     }
 
-    // overloading calcFeetAndInchesToCentimeters()
-    public double calcFeetAndInchesToCentimeters(double inches) {
-        if (inches >= 0) {
-            feet = inches / 12;
-            double remainingInches = inches % 12;
-            return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    // method to convert feet and inches to centimeters
+    // 2nd method, first overloaded
+    public double calcFeetAndInchesToCentimeters(double feet2, double inches2) {
+        if (feet2 >= 0 && inches2 >= 0 && inches2 < 12) {
+            return ((feet2 * 12) * 2.54) + (inches2 * 2.54);
         } else {
             return -1;
         }
     }
 
-    // overloaded method using values given through constructor
-    public double calcFeetAndInchesToCentimeters() {
-        if (feet >= 0 && inches >= 0 && inches <= 12) {
-            return inches * 2.54;
+    // overloading calcFeetAndInchesToCentimeters()
+    // 3rd method, second overloaded
+    public double calcFeetAndInchesToCentimeters(double inches3) {
+        if (inches3 >= 0) {
+            double feet3 = inches3 / 12;
+            double remainingInches = inches3 % 12;
+            return calcFeetAndInchesToCentimeters(feet3, remainingInches);
         } else {
             return -1;
         }
