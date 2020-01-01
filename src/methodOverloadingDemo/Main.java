b6cd65cb82f8d12f957f3a85937b77b43fb47015
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-01 16:04:12 +0530

    improved code readability

--- src/methodOverloadingDemo/Main.java
+++ src/methodOverloadingDemo/Main.java
@@ -6,12 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         // creating object of class and using its constructor
+        // calling original method
         ConversionToCentimeters obj = new ConversionToCentimeters(4, 3);
-        double centimeters3 = obj.calcFeetAndInchesToCentimeters();
-        if (centimeters3 < 0.0) {
+        double centimeters = obj.calcFeetAndInchesToCentimeters();
+        if (centimeters < 0.0) {
             System.out.println("Invalid input");
         } else {
-            System.out.println(centimeters3);
+            System.out.println(centimeters);
         }
 
         // taking user input
@@ -21,21 +22,20 @@ public class Main {
         System.out.print("Enter inch: ");
         double inches = scan.nextDouble();
 
-        // calling method with parameters and output
-        double centimeters = obj.calcFeetAndInchesToCentimeters(feet, inches);
-        if (centimeters < 0.0) {
-            System.out.println("Invalid input");
-        } else {
-            System.out.println(feet + " feet " + inches + " inches = " + centimeters + " cm");
-        }
-
-        // calling method with only 1 parameter and printing output
-        double centimeters2 = obj.calcFeetAndInchesToCentimeters(inches);
+        // calling first overloaded method
+        double centimeters2 = obj.calcFeetAndInchesToCentimeters(feet, inches);
         if (centimeters2 < 0.0) {
             System.out.println("Invalid input");
         } else {
-            System.out.println(inches + " inches = " + centimeters + " cm");
+            System.out.println(feet + " feet " + inches + " inches = " + centimeters2 + " cm");
         }
 
+        // calling second overloaded method
+        double centimeters3 = obj.calcFeetAndInchesToCentimeters(inches);
+        if (centimeters3 < 0.0) {
+            System.out.println("Invalid input");
+        } else {
+            System.out.println(inches + " inches = " + centimeters3 + " cm");
+        }
     }
 }
