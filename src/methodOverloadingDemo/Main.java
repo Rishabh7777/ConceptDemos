84970b325a3dab6c47ed7967e406c98e60ce965f
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-04 15:20:25 +0530

    add Scanner class

--- src/methodOverloadingDemo/Main.java
+++ src/methodOverloadingDemo/Main.java
@@ -1,7 +1,17 @@
 package methodOverloadingDemo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        // taking user input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter feet: ");
+        double feet = scan.nextDouble();
+        System.out.println("Enter inches: ");
+        double inches = scan.nextDouble();
+
 
     }
 }
