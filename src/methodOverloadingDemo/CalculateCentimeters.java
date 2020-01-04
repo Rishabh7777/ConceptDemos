bd0ae0343b36fd83695afa456dcdfc66e03a9c61
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-04 15:07:22 +0530

    add getter and setter

--- src/methodOverloadingDemo/CalculateCentimeters.java
+++ src/methodOverloadingDemo/CalculateCentimeters.java
@@ -2,12 +2,30 @@ package methodOverloadingDemo;
 
 public class CalculateCentimeters {
 
-    double feet;
-    double inches;
+    private double feet;
+    private double inches;
 
     // constructor
     public CalculateCentimeters(double feet, double inches) {
         this.feet = feet;
         this.inches = inches;
     }
+
+    // getters and setters
+    public double getFeet() {
+        return feet;
+    }
+
+    public void setFeet(double feet) {
+        this.feet = feet;
+    }
+
+    public double getInches() {
+        return inches;
+    }
+
+    public void setInches(double inches) {
+        this.inches = inches;
+    }
+
 }
