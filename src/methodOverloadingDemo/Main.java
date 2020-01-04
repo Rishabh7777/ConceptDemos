146478633361890e08d9f6360a344b0b5e77fc63
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-04 15:25:07 +0530

    add method calling for calcFeetAndInchesToCentimeters() with two parameters

--- src/methodOverloadingDemo/Main.java
+++ src/methodOverloadingDemo/Main.java
@@ -7,11 +7,16 @@ public class Main {
 
         // taking user input
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter feet: ");
+        System.out.println("Enter feet and inches: ");
         double feet = scan.nextDouble();
-        System.out.println("Enter inches: ");
         double inches = scan.nextDouble();
 
-
+        // calling method with two parameters
+        double centimeters = CalculateCentimeters.calcFeetAndInchesToCentimeters(feet, inches);
+        if (centimeters < 0) {
+            System.out.println("Invalid feet/inches value.");
+        } else {
+            System.out.println(feet + " feet " + inches + " inches = " + centimeters + " cms");
+        }
     }
 }
