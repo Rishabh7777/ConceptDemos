b288eaf01fe967adb2379775d0e5527cc407b608
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-04 15:01:07 +0530

    add constructor

--- src/methodOverloadingDemo/CalculateCentimeters.java
+++ src/methodOverloadingDemo/CalculateCentimeters.java
@@ -1,4 +1,13 @@
 package methodOverloadingDemo;
 
 public class CalculateCentimeters {
+
+    double feet;
+    double inches;
+
+    // constructor
+    public CalculateCentimeters(double feet, double inches) {
+        this.feet = feet;
+        this.inches = inches;
+    }
 }
