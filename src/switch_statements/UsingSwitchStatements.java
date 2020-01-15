ea4d2106caf9fb8457f76dcaad97a877ca60935a
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-15 19:55:01 +0530

    add switch statements

--- src/switch_statements/UsingSwitchStatements.java
+++ src/switch_statements/UsingSwitchStatements.java
@@ -7,10 +7,36 @@ public class UsingSwitchStatements {
 
         // taking user input
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a alphabet:");
+        System.out.println("Enter a alphabet in uppercase:");
         char c = scan.next().charAt(0);     // way to take char input, it takes input upto first space only
 //        String str = scan.next();       // input using next() stops at space
         System.out.println(c);
 //        System.out.println(str);
+
+        // applying switch statements
+        switch (c) {
+            case 'A':
+                System.out.println("A for Apple");
+                break;
+
+            case 'B':
+                System.out.println("B for Ball");
+                break;
+
+            case 'C':
+                System.out.println("C for Cat");
+                break;
+
+            case 'D':
+                System.out.println("D for Dog");
+                break;
+
+            case 'E':
+                System.out.println("E for Elephant");
+                break;
+
+            default:
+                System.out.println("Oops! I have not learnt that many");
+        }
     }
 }
