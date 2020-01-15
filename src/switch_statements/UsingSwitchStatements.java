1a78b9ab131c465286d69c87909cb72603b7a786
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-01-15 19:49:16 +0530

    add Scanner class

--- src/switch_statements/UsingSwitchStatements.java
+++ src/switch_statements/UsingSwitchStatements.java
@@ -1,7 +1,16 @@
 package switch_statements;
 
+import java.util.Scanner;
+
 public class UsingSwitchStatements {
     public static void main(String[] args) {
 
+        // taking user input
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a alphabet:");
+        char c = scan.next().charAt(0);     // way to take char input, it takes input upto first space only
+//        String str = scan.next();       // input using next() stops at space
+        System.out.println(c);
+//        System.out.println(str);
     }
 }
