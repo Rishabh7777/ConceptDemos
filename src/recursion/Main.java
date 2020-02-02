3fa979b117690b5945c7bad0b681feaa1127bf05
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-02-02 12:03:42 +0530

    add Scanner class and method calling

--- src/recursion/Main.java
+++ src/recursion/Main.java
@@ -1,7 +1,15 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter element to print fibonacci series: ");
+        int element = scan.nextInt();
+
+        FibonacciSeries obj = new FibonacciSeries();
+        obj.printFibonacciSeries(element);
     }
 }
