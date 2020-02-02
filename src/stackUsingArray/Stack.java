6e043f72df088f2aefe2155d648f5604cb3e1d4f
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-02-02 17:51:39 +0530

    add constructor

--- src/stackUsingArray/Stack.java
+++ src/stackUsingArray/Stack.java
@@ -1,4 +1,11 @@
 package stackUsingArray;
 
 public class Stack {
+
+    private int[] arrStack;     // array for stack
+
+    // constructor
+    public Stack(int sizeOfStack) {
+        arrStack = new int[sizeOfStack];
+    }
 }
