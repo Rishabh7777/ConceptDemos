62ab8a16de278269bf278ebd148855c1d86fbd03
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-02-02 12:00:47 +0530

    add method printFibonacciSeries which follows iteration

--- src/recursion/FibonacciSeries.java
+++ src/recursion/FibonacciSeries.java
@@ -1,4 +1,18 @@
 package recursion;
 
 public class FibonacciSeries {
+
+    private int element1 = 1;
+    private int element2 = 1;
+    private int element3;
+
+    // defining method to find fibonacci using iteration
+    public void printFibonacciSeries(int element) {
+        for (int i = 0; i < element; i++) {
+            element3 = element1 + element2;
+            System.out.print(element3 + "\t");
+            element1 = element2;
+            element2 = element3;
+        }
+    }
 }
