08db81fa95192cf50226b24f555f347e0873f8c3
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-02-02 12:54:19 +0530

    add method fibonacciNumber with recursion

--- src/recursion/FibonacciSeries.java
+++ src/recursion/FibonacciSeries.java
@@ -8,13 +8,24 @@ public class FibonacciSeries {
 
     // defining method to find fibonacci using iteration
     public void printFibonacciSeries(int element) {
-        System.out.print(element1 + "\t");
-        System.out.print(element2 + "\t");
-        for (int i = 2; i < element; i++) {
-            element3 = element1 + element2;
-            System.out.print(element3 + "\t");
-            element1 = element2;
-            element2 = element3;
+        if (element == 1) {
+            System.out.print(element1 + "\t");
+        } else {
+            System.out.print(element1 + "\t");
+            System.out.print(element2 + "\t");
+            for (int i = 2; i < element; i++) {
+                element3 = element1 + element2;
+                System.out.print(element3 + "\t");
+                element1 = element2;
+                element2 = element3;
+            }
         }
+    }
+
+    // method to print fibonacci using recursion
+    public int fibonacciNumber(int n) {
+        if (n <= 1)
+            return n;
+        return fibonacciNumber(n - 1) + fibonacciNumber(n - 2);
     }
 }
