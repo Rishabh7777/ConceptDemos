fb6a2beb33af9ab9beb101d103b2b385afdda31e
Rishabh Tiwari <rishabh.tiwari_cs18@gla.ac.in>
2020-02-02 18:08:36 +0530

    add methods push() and isFull()

--- src/stackUsingArray/Stack.java
+++ src/stackUsingArray/Stack.java
@@ -2,10 +2,29 @@ package stackUsingArray;
 
 public class Stack {
 
-    private int[] arrStack;     // array for stack
+    private int top;
+    private int[] arr;     // array for stack
+    private int maxSize;
 
     // constructor
     public Stack(int sizeOfStack) {
-        arrStack = new int[sizeOfStack];
+        arr = new int[sizeOfStack];
+        top = 0;
+        maxSize = sizeOfStack;
+    }
+
+    // for inserting new element
+    void push(int element) {
+        if (!isFull()) {
+            arr[top] = element;
+            top++;
+        } else {
+            System.out.println("Stack overflow");
+        }
+    }
+
+    // for checking if stack is full or not
+    private boolean isFull() {
+        return arr.length == maxSize;
     }
 }
